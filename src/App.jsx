00ab84e69ed3e0cb1f5b3f106c089866f4f17114
hamimahmad <hamimahmad027@gmail.com>
2026-03-03 02:26:26 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Banner from './Components/Banner'
+import Navbar from './Components/Navbar'
+
+function App() {
+  
+  return (
+    <>
+      <Navbar/>
+      <Banner/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div>This is Navbar</div>
+    </div>
+  )
+}
+
+export default Navbar
