99d77f956a01aba65dcacfe160469a7b15fa2b28
hamimahmad <hamimahmad027@gmail.com>
2026-03-03 03:25:21 +0600

    ...

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,18 @@
 const Navbar = () => {
   return (
     <div>
-      <div>This is Navbar</div>
+      <div>
+        <nav className="flex justify-between items-center p-5 bg-blue-300">
+            <div className="text-4xl font-bold"> LOGO</div>
+            <ul className="flex justify-between gap-20 text-xl ">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">About </a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <button className="text-white bg-blue-600 p-3 rounded-2xl font-bold cursor-pointer">Join Now</button>
+        </nav>
+      </div>
     </div>
   )
 }
