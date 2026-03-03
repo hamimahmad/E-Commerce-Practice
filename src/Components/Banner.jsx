23a8f943d2ddf6e822eb26ac1bd548db15756438
hamimahmad <hamimahmad027@gmail.com>
2026-03-03 23:56:31 +0600

    0303261156

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>
-      This is Banner
+    <div className='container bg-amber-400 mx-auto p-10 '>
+      <h2 className='text-3xl text-center '> This is Banner </h2>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <div>
       <div>
         <nav className="flex justify-between items-center p-5 bg-blue-300">
-            <div className="text-4xl font-bold"> LOGO</div>
+            <div className="text-4xl font-bold"> <a href="#">LOGO</a> </div>
             <ul className="flex justify-between gap-20 text-xl ">
                 <li><a href="#">Home</a></li>
                 <li><a href="#">Service</a></li>
