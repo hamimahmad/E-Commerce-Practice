68f35fb7b82bce4adb3b1b8e6e98664e50372120
hamimahmad <hamimahmad027@gmail.com>
2026-03-05 22:42:36 +0600

    ...

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className='container bg-amber-400 mx-auto p-10 '>
-      <h2 className='text-3xl text-center '> This is Banner </h2>
+      <h1 className='text-3xl text-center '> This is Banner </h1>
+      <p className='text-center p-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus natus earum magni molestias voluptate? Culpa consequatur deleniti dolore! Adipisci voluptates eos reiciendis minima? Incidunt maiores nihil, expedita eos nisi alias.</p>
     </div>
   )
 }
