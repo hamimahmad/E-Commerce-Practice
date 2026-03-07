13dcb986998c26a7823723f86ef1351c720caf58
hamimahmad <hamimahmad027@gmail.com>
2026-03-07 23:05:44 +0600

    m7 2026

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../Components/Header'
+import Banner from '../Components/Banner'
 
 const Home = () => {
   return (
     <>
       <Header/>
+      <Banner/>
     </>
   )
 }
