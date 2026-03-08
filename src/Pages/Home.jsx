b0d5b2c459ee3e860385f3a79cafa498b35a9459
hamimahmad <hamimahmad027@gmail.com>
2026-03-08 06:57:45 +0600

    ...

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Banner from '../Components/Banner'
+import Navbar from '../Components/Navbar'
 
 const Home = () => {
   return (
     <>
       <Header/>
+      <Navbar/>
       <Banner/>
     </>
   )
