9ed0781cbeb599f4d6e5b9d8024f9120cade8233
hamimahmad <hamimahmad027@gmail.com>
2026-03-14 02:40:32 +0600

    reset and subs button added

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -4,13 +4,27 @@ const Counter = () => {
     const [count, setCount] = useState(0);
 
     const handleAdd = () =>{
-        console.log("its working")
+        const newCount = count+5
+        setCount(newCount)
+    }
+
+    const handleReset = () => {
+        setCount(0)
+        
+    }
+
+    const handleSubs = () => {
+        const subs = count-5 
+        setCount(subs)
     }
 
   return (
     <div className='border-2 border-white p-5'>
       <h3 className='text-white font-bold text-xl'>Count : {count} </h3>
       <button onClick={handleAdd} className='bg-white px-4 py-2 my-3'>Add</button>
+      <button onClick={handleReset} className='bg-white px-4 py-2 my-3 ml-3'>Reset</button>
+      <button onClick={handleSubs} className='bg-white px-4 py-2 my-3 ml-3'>Substract</button>
+
     </div>
   )
 }
