1623899887a933649eac3894a86ad9225ba46ef1
hamimahmad <hamimahmad027@gmail.com>
2026-03-13 22:38:35 +0600

    ...

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <footer className='bg-red-400 '>
-      <h4 className='mx-auto container py-10'>
-        this it the Footer
-      </h4>
+    <footer className="bg-red-400 ">
+      <div className="mx-auto container py-10">
+        <div className="flex justify-between">
+          <h4>this it the Footer</h4>
+          <ul>
+            <li>
+              <h5 className="text-lg font-bold">Navigations</h5>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Service</a>
+            </li>
+            <li>
+              <a href="#">About </a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+          <div>
+            <h5 className="text-lg font-bold">Contacts</h5>
+            <p>Monipur, Mirpur-2, Dhaka-1216</p>
+          </div>
+        </div>
+      </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
